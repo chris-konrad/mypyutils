ed7f2e784b37316009cf97e7e4d8c90acbe5c964
Christoph M. Konrad <c.m.konrad@tudelft.nl>
2025-12-18 09:09:11 +0100

    New plot module with subplots from aspect.

---
+++ src/mypyutils/plot.py
@@ -0,0 +1,35 @@
+"""
+Created on Thu Dec 18 09:02:26 2025
+
+Plotting functions
+
+@author: Christoph M. Konrad
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def subplots_from_aspect(n_plots, aspect=1.78, **kwargs):
+    """Create a grid of suplots with the given aspect ratio fitting at least n_plots. 
+
+    Parameters
+    ----------
+    n_plots : int
+        Number of plots. The resulting grid will fit at least this number.
+    aspect : float, optional
+        The ratio of columns to rows, i.e. aspect = ncols/nrows. The default is 16:9.
+
+    Returns
+    -------
+    fig
+        Matplotlib figure
+    axes
+        Array of axes. 
+    """
+
+    rows = np.ceil(np.sqrt(n_plots/aspect))
+    cols = np.ceil(n_plots/rows)
+
+    fig, axes = plt.subplots(int(rows), int(cols), **kwargs)
+
+    return fig, axes
